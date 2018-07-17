9444a8130b61c9a8461d0cd692b731b4ae02a9b5
michaelz_015 <zqebsr@126.com>
2018-07-17 22:21:49 +0800

    antd module configuration

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,79 +1,42 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Table } from 'antd';
+import * as styles from './Main.less';
+
 // import d3 from 'd3';
-
+const COLUMNS = [
+    {
+        key: 'a',
+        title: 'a',
+        dataIndex: 'a'
+    },
+    {
+        key: 'b',
+        title: 'b',
+        dataIndex: 'a'
+    }
+];
 export default class Main extends React.Component {
-    componentDidMount() {
-        // this.renderTriangle();
-        this.renderRing();
-    }
 
-    renderTriangle() {
-        d3.select('#rect').append('rect')
-            .attr('id', 'myrect')
-            .attr('width', '100px')
-            .attr('height', '100px');
-        d3.select('#myrect')
-            .style('border-radius', '10px');
-    }
-    renderRing() {
-        var dataset = [40, 20];
-
-        //(1)转化数据为适合生成饼图的对象数组
-        var pie = d3.layout.pie(dataset);
-        console.log(pie);
-        var h = 300;
-        var w = 300;
-
-        var outerRadius = w / 2;//外半径
-
-        //(7)圆环内半径
-        var innerRadius = w / 3;
-
-        //(2)用svg的path绘制弧形的内置方法
-        var arc = d3.svg.arc()//设置弧度的内外径，等待传入的数据生成弧度
-            .outerRadius(outerRadius)
-            .innerRadius(innerRadius)
-            .cornerRadius(50)
-            .padAngle(0.3);
-
-        var svg = d3.select('body')
-            .append('svg')
-            .attr('width', w)
-            .attr('height', h);
-
-        //(3)颜色函数
-        var color = d3.scale.category10();//创建序数比例尺和包括10中颜色的输出范围
-
-        //(4)准备分组,把每个分组移到图表中心
-        var arcs = svg.selectAll('g.arc')
-            .data(pie(dataset))
-            .enter()
-            .append('g')
-            .attr('class', 'arc')
-            //移到图表中心
-            .attr('transform', 'translate(' + outerRadius + ',' + outerRadius + ')');//translate(a,b)a表示横坐标起点，b表示纵坐标起点
-
-        //(5)为组中每个元素绘制弧形路路径
-        arcs.append('path')//每个g元素都追加一个path元素用绑定到这个g的数据d生成路径信息
-            .attr('fill', function (d, i) {//填充颜色
-                return color(i);
-            })
-            .attr('d', arc);//将角度转为弧度（d3使用弧度绘制）
-
-        //(6)为组中每个元素添加文本
-        arcs.append('text')//每个g元素都追加一个path元素用绑定到这个g的数据d生成路径信息
-            .attr('transform', function (d) {
-                return 'translate(' + arc.centroid(d) + ')';//计算每个弧形的中心点（几何中心）
-            })
-            .attr('text-anchor', 'middle')
-            .text(function (d) {
-                return d.value;//这里已经转为对象了
-            });
-    }
     render() {
+        const data = [
+            {
+                id: 1,
+                a: 111111,
+                b: 22222
+            },
+            {
+                id: 2,
+                a: 111111,
+                b: 22222
+            }
+        ];
         return (
-            <div className='bg'>
+            <div className={styles.bg}>
+                <Table
+                    columns={COLUMNS}
+                    rowKey={(record) => record.id}
+                    dataSource={data}
+                />
             </div>
         );
     };
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
 import Main from './Main.jsx';
 
 ReactDOM.render(
-    <div className="helloworld">
-        <Main />
-    </div>, document.getElementById('root'),
+    <Main />, document.getElementById('root'),
 );
